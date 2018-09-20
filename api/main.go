ef9c3ae091df569cfb0d1072c2f49dfebb24ff20
Brian Seymour <imbrianseymour@gmail.com>
2018-09-20 12:04:34 -0500

    add additional executors

--- api/main.go
+++ api/main.go
@@ -2,13 +2,13 @@ package main
 
 import (
     "bytes"
-    "fmt"
-    "time"
     "encoding/json"
-    "net/http"
+    "fmt"
     "io/ioutil"
+    "net/http"
     "os/exec"
     "strings"
+    "time"
 )
 
 type inbound struct {
@@ -33,6 +33,7 @@ func main() {
 }
 
 func Execute(res http.ResponseWriter, req *http.Request) {
+    var found = false
     res.Header().Set("Content-Type", "application/json")
 
     // get json
@@ -41,26 +42,28 @@ func Execute(res http.ResponseWriter, req *http.Request) {
     message.Decode(&inbound)
 
     whitelist := []string{
-        "python2",
         "python",
+        "python2",
         "python3",
         "ruby",
         "javascript",
         "js",
         "node",
+        "c",
+        "cpp",
+        "c++",
     }
 
-    found := false
-
     // check if the supplied language is supported
+    // now calls function and returns
     for _, lang := range whitelist {
         if lang == inbound.Language {
-            found = true
-            break
+            launch(inbound, res)
+            return
         }
     }
 
-    // send an error if it isn't
+    // now only called when the language is not supported
     if !found {
         problem := problem{
             Code: "unsupported_language",
@@ -72,14 +75,16 @@ func Execute(res http.ResponseWriter, req *http.Request) {
         res.Write(pres)
         return
     }
+}
 
+func launch(inbound2 inbound, res http.ResponseWriter){
     // write the code to temp dir
     filename := fmt.Sprintf("/tmp/%d.code", time.Now().UnixNano())
 
-    ioutil.WriteFile(filename, []byte(inbound.Source), 0644)
+    ioutil.WriteFile(filename, []byte(inbound2.Source), 0644)
 
     // set up the execution
-    cmd := exec.Command("../docker/execute", inbound.Language, filename)
+    cmd := exec.Command("../docker/execute", inbound2.Language, filename)
 
     // capture out/err
     var stdout, stderr bytes.Buffer
