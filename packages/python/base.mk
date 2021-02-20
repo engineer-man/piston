13d5191d77af62542a34a871e3b0ba4b27b9ad31
Thomas Hobson <git@hexf.me>
2021-02-20 18:34:44 +1300

    pkg(python): refactor

--- packages/python/base.mk
+++ packages/python/base.mk
@@ -1,7 +1,5 @@
-NAME=python
 AUTHOR=Thomas Hobson <thomas@hexf.me>
 DEPS=
-#VERSION=
 COMPILED=false
 
 include ../../common.mk
@@ -9,13 +7,13 @@ include ../../common.mk
 run:
 	echo 'python$(shell grep -oP "\d+.\d+"<<<${VERSION}) $$*' > run
 
-python-${VERSION}/environment:
+${NAME}-${VERSION}/environment:
 	echo 'export PATH=$$PWD/${NAME}-${VERSION}/bin:$$PATH' > $@
 
-python-${VERSION}/: Python-${VERSION}/
+${NAME}-${VERSION}/: Python-${VERSION}/
 	cd $< && ./configure --prefix /
 	$(MAKE) -j$(or ${MAKE_JOBS},64) -C $<
 	DESTDIR=../$@ $(MAKE) -j$(or ${MAKE_JOBS},64) -C $< altinstall || true
 
-Python-${VERSION}.tgz: 
+${NAME}-${VERSION}.tgz: 
 	curl "https://www.python.org/ftp/python/${VERSION}/$@" -o $@
---
+++ packages/secondary.mk
@@ -0,0 +1,22 @@
+.PHONY: build sign cleanup clean
+build: $(patsubst %,%/${LANGUAGE}-%.pkg.tar.gz,${VERSIONS})
+sign: $(patsubst %,%/${LANGUAGE}-%.pkg.tar.gz.asc,${VERSIONS})
+clean:
+	rm -rf ${VERSIONS}
+cleanup: $(patsubst %,%/cleanup,${VERSIONS})
+	
+
+%/cleanup: %/Makefile
+	$(MAKE) -C $(shell dirname $<) cleanup
+	rm $(shell dirname $<)/Makefile
+
+%/${LANGUAGE}-%.pkg.tar.gz.asc: %/Makefile
+	$(MAKE) -C $(shell dirname $<) sign
+%/${LANGUAGE}-%.pkg.tar.gz: %/Makefile
+	$(MAKE) -C $(shell dirname $<)
+
+%/Makefile: 
+	@mkdir -p $(shell dirname $@)
+	@echo 'VERSION=$(patsubst %/Makefile,%,$@)' > $@
+	@echo 'NAME=${LANGUAGE}' > $@
+	@echo 'include ../base.mk' >> $@
