51666c2d5ae633fbea8a9239827d8ae59734d7c3
Thomas Hobson <git@hexf.me>
2021-03-06 01:06:00 +1300

    pkg(mono-6.12.0): change to new build system

---
+++ packages/mono-6.12.0/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -p build/tmp build/mono
+cd build
+
+curl "https://download.mono-project.com/sources/mono/mono-6.12.0.122.tar.xz" -o mono.tar.xz
+tar xf mono.tar.xz --strip-components=1 -C mono
+
+cd mono
+
+./configure --prefix /piston/packages/mono/6.12.0/mono-6.12.0
+
+make
+DESTDIR=build/tmp make install
+
+mv build/tmp/piston/packages/mono/6.12.0/mono-6.12.0 ../../output
