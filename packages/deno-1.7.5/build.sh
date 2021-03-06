81dd0acbd00c08b182005e9553834711c9849324
Thomas Hobson <git@hexf.me>
2021-03-06 19:29:43 +1300

    pkg(deno-1.7.5): update to match

--- packages/deno-1.7.5/build.sh
+++ packages/deno-1.7.5/build.sh
@@ -1,11 +1,5 @@
-mkdir -p output build
-
-cd build
 curl -L https://github.com/denoland/deno/releases/download/v1.7.5/deno-x86_64-unknown-linux-gnu.zip --output deno.zip
-unzip deno.zip
+unzip -o deno.zip
+rm deno.zip
 
-cd ..
-
-mv build/deno output
-
-chmod +x output/deno
+chmod +x deno
