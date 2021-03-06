6391120176e36bbd5320d543cafb9294d319b08e
Thomas Hobson <git@hexf.me>
2021-03-06 19:31:02 +1300

    pkg(node-15.10.0): update to match

--- packages/node-15.10.0/build.sh
+++ packages/node-15.10.0/build.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-mkdir -p build output
-
-cd build
-
 curl "https://nodejs.org/dist/v15.10.0/node-v15.10.0-linux-x64.tar.xz" -o node.tar.xz
 tar xf node.tar.xz --strip-components=1
-
-cd ..
-mv build/bin/node output
+rm node.tar.xz
