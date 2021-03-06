a24770e43a368d0f15afd8b0fa01c074c6eb0296
Thomas Hobson <git@hexf.me>
2021-03-06 19:38:52 +1300

    pkg(python-3.9.1): match new build system

--- packages/python-3.9.1/build.sh
+++ packages/python-3.9.1/build.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
-mkdir -p build/tmp build/python
-cd build
-
 curl "https://www.python.org/ftp/python/3.9.1/Python-3.9.1.tgz" -o python.tar.gz
-tar xzf python.tar.gz --strip-components=1 -C python
-
-cd python
-
+tar xzf python.tar.gz --strip-components=1
+rm python.tar.gz
 
 ./configure --prefix /piston/packages/python/3.9.1/python-3.9.1
 make -j$(nproc)
-DESTDIR=build/tmp make altinstall -j$(nproc)
-
-
-mv build/tmp/piston/packages/python/3.9.1/python-3.9.1 ../../output
-
+ln -s python python3.9
 
