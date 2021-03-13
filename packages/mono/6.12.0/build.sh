509013f4a0e35cf8c8c70e0ba6ddca27bbe0f4db
Thomas Hobson <git@hexf.me>
2021-03-13 19:53:00 +1300

    pkg(mono-6.12.0): relocate files

---
+++ packages/mono/6.12.0/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+PREFIX=$(realpath $(dirname $0))
+
+mkdir -p build/mono
+cd build
+
+curl "https://download.mono-project.com/sources/mono/mono-6.12.0.122.tar.xz" -o mono.tar.xz
+tar xf mono.tar.xz --strip-components=1 -C mono
+
+cd mono
+
+./configure --prefix "$PREFIX"
+
+make -j$(nproc) 
+make install -j$(nproc)
+
+cd ../../
+rm -rf build
