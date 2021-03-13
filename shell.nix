69c013fa7a860ecc73e64775689ed053e237d66f
Thomas Hobson <git@hexf.me>
2021-03-13 14:31:40 +1300

    dev: nix shell file

---
+++ shell.nix
@@ -0,0 +1,5 @@
+{ pkgs ? import <nixpkgs> {} }:
+  pkgs.mkShell {
+    # nativeBuildInputs is usually what you want -- tools you need to run
+    nativeBuildInputs = [ pkgs.buildPackages.nodejs-15_x ];
+}
