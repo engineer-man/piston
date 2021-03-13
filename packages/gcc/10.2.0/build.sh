f852b60b69996b0bca211ebdf04b0812dcfbc7d4
Thomas Hobson <git@hexf.me>
2021-03-13 23:12:50 +1300

    pkg(gcc-10.2.0): Add c, c++ and D via gcc 10.2.0

---
+++ packages/gcc/10.2.0/build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Put instructions to build your package in here
+[[ -d "bin" ]] && exit 0
+PREFIX=$(realpath $(dirname $0))
+
+mkdir -p build obj
+
+cd build
+
+curl "https://ftp.gnu.org/gnu/gcc/gcc-10.2.0/gcc-10.2.0.tar.gz" -o gcc.tar.gz
+
+tar xzf gcc.tar.gz --strip-components=1
+
+./contrib/download_prerequisites
+
+cd ../obj
+
+# === autoconf based === 
+../build/configure --prefix "$PREFIX" --enable-languages=c,c++,d --disable-multilib --disable-bootstrap
+
+make -j$(nproc)
+make install -j$(nproc)
+cd ../
+rm -rf build obj
---
+++ packages/gcc/10.2.0/test.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(void) {
+    printf("OK");
+    return 0;
+}
