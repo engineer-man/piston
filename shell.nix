e5000917e8b5ccb701bee427fa0f92748c1b90a0
Thomas Hobson <git@hexf.me>
2021-03-13 15:58:30 +1300

    dev(nix-shell): add yarn

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
-    nativeBuildInputs = [ pkgs.buildPackages.nodejs-15_x ];
+    nativeBuildInputs = [ pkgs.nodejs-15_x pkgs.yarn ];
 }
