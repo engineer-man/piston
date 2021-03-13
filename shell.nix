535ce6ce97777a2ef059833cf8a57f121319a40c
Thomas Hobson <git@hexf.me>
2021-03-13 19:53:57 +1300

    dev(shell.nix): add jq to shell

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
-    nativeBuildInputs = [ pkgs.nodejs-15_x pkgs.yarn ];
+    nativeBuildInputs = [ pkgs.nodejs-15_x pkgs.yarn pkgs.jq ];
 }
