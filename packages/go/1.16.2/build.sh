4dade61599d59c941ff373198be050e07562bada
Victor Frazao <luiz_victor_frazao@hotmail.com>
2021-03-14 10:44:26 -0400

    pkg(go-1.16.2): Fix build.sh to follow redirects

--- packages/go/1.16.2/build.sh
+++ packages/go/1.16.2/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-curl https://golang.org/dl/go1.16.2.linux-amd64.tar.gz -o go1.16.2.linux-amd64.tar.gz
+curl -LO https://golang.org/dl/go1.16.2.linux-amd64.tar.gz
 tar -xzf go1.16.2.linux-amd64.tar.gz
 rm go1.16.2.linux-amd64.tar.gz
