be1b93cf1f2f1a735fe50fc91d7e1148b4f37f03
Thomas Hobson <git@hexf.me>
2021-03-14 14:50:55 +1300

    repo: add CI mode

--- repo/entrypoint.sh
+++ repo/entrypoint.sh
@@ -3,16 +3,27 @@ cd /piston/packages
 SERVER=1
 BUILD=1
 
+ls -la /piston /piston/*
+
 echo "Running through arguments.."
 
 for pkg in "$@"
 do
+    shift
     if [[ "$pkg" = "--no-server" ]]; then
         echo "Not starting index server after builds"
         SERVER=0
     elif [[ "$pkg" = "--no-build" ]]; then
         echo "Building no more package"
         BUILD=0
+    elif [[ "$pkg" = "--ci" ]]; then
+        echo "Running in CI mode, --no-build, --no-server"
+        BUILD=0
+        SERVER=0
+        for package in "$(git diff-tree --no-commit-id --name-only -r $1 | awk -F/ '{ print $2 "-" $3 }' | sort -u)"; do
+            make -j16 $package.pkg.tar.gz
+        done
+
     else
         if [[ $BUILD -eq 1 ]]; then
             echo "Building package $pkg"
