225960640919601853b66f88d33c3c6541264254
Thomas <thomas@hexf.me>
2021-03-14 17:47:20 +1300

    Update build.sh

--- packages/go/1.16.2/build.sh
+++ packages/go/1.16.2/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-wget https://golang.org/dl/go1.16.2.linux-amd64.tar.gz
+curl https://golang.org/dl/go1.16.2.linux-amd64.tar.gz -o go1.16.2.linux-amd64.tar.gz
 tar -xzf go1.16.2.linux-amd64.tar.gz
 rm go1.16.2.linux-amd64.tar.gz
