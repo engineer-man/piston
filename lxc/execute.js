16c585d41d649af8a1be4cd35559d60970f27339
Brian Seymour <imbrianseymour@gmail.com>
2021-03-14 22:08:56 -0500

    dont trim output

--- lxc/execute.js
+++ lxc/execute.js
@@ -37,9 +37,9 @@ function execute(language, source, stdin = '', args = []) {
         });
 
         process.on('exit', code => {
-            stderr = stderr.trim().substring(0, OUTPUT_LIMIT);
-            stdout = stdout.trim().substring(0, OUTPUT_LIMIT);
-            output = output.trim().substring(0, OUTPUT_LIMIT);
+            stderr = stderr.substring(0, OUTPUT_LIMIT);
+            stdout = stdout.substring(0, OUTPUT_LIMIT);
+            output = output.substring(0, OUTPUT_LIMIT);
 
             resolve({
                 stdout,
