fd8e25608ff94b88599ddef06da01b9723a081ad
Thomas Hobson <git@hexf.me>
2021-03-15 19:24:59 +1300

    pkg: bump all binary download packages

--- packages/go/1.16.2/build.sh
+++ packages/go/1.16.2/build.sh
@@ -2,3 +2,4 @@
 curl -LO https://golang.org/dl/go1.16.2.linux-amd64.tar.gz
 tar -xzf go1.16.2.linux-amd64.tar.gz
 rm go1.16.2.linux-amd64.tar.gz
+
--- packages/julia/1.5.4/build.sh
+++ packages/julia/1.5.4/build.sh
@@ -3,3 +3,4 @@
 curl -OL https://julialang-s3.julialang.org/bin/linux/x64/1.5/julia-1.5.4-linux-x86_64.tar.gz
 tar zxvf julia-1.5.4-linux-x86_64.tar.gz
 rm julia-1.5.4-linux-x86_64.tar.gz
+
