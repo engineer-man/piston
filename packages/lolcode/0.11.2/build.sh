228880ba9bda2fcc76f90c6970816902fb384dfc
Shivansh-007 <69356296+Shivansh-007@users.noreply.github.com>
2021-03-19 03:55:25 +0530

    pkg(lolcode-0.11.2): Shift to cmake/make rather then running install.py

--- packages/lolcode/0.11.2/build.sh
+++ packages/lolcode/0.11.2/build.sh
@@ -7,7 +7,9 @@ git clone https://github.com/justinmeza/lci.git lolcode
 cd lolcode
 
 # Building and installing lolcode
-./install.py --prefix="$PREFIX"
+cmake -DCMAKE_INSTALL_PREFIX:STRING="$PREFIX" .
+make -j$(nproc)
+make install -j$(nproc)
 
 # Cleaning up
 cd ../ && rm -rf lolcode
