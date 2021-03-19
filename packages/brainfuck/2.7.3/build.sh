fed0a3eeaf330a708bce6befd086c62b767aa806
Thomas Hobson <git@hexf.me>
2021-03-19 22:34:41 +1300

    pkg(brainfuck-2.7.3): relocate fs tree

--- packages/brainfuck/2.7.3/build.sh
+++ packages/brainfuck/2.7.3/build.sh
@@ -3,16 +3,17 @@
 # Put instructions to build your package in here
 PREFIX=$(realpath $(dirname $0))
 
-mkdir -p build
-
 git clone https://github.com/fabianishere/brainfuck.git
 
+cd brainfuck
+
+mkdir -p build
 cd build
 
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX/ -DENABLE_EDITLINE=OFF ../brainfuck
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX/ -DENABLE_EDITLINE=OFF ..
 
 make -j$(nproc)
 make install -j$(nproc)
 
-cd ../
-rm -rf build brainfuck
+cd ../../
+rm -rf brainfuck
