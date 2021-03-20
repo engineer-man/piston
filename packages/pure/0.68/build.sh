a643f15062d850f3bb36299d4414a5102fb14905
Shivansh-007 <Shivansh-007@users.noreply.github.com>
2021-03-20 05:56:50 +0530

    pkg(pure-0.68): Remove `--strip-components` flag and use SemVer.

--- packages/pure/0.68/build.sh
+++ packages/pure/0.68/build.sh
@@ -3,14 +3,15 @@
 PREFIX=$(realpath $(dirname $0))
 
 # Installing LLVM and clang (the latter is optional but recommended)
-curl "https://releases.llvm.org/3.4.2/llvm-3.4.2.src.tar.gz" -o llvm.tar.gz
-curl "https://releases.llvm.org/3.4.2/cfe-3.4.2.src.tar.gz" -o clang.tar.gz
-tar xfz llvm.tar.gz --strip-components=1
-tar xfz clang.tar.gz --strip-components=1
+curl "http://llvm.org/releases/3.4/llvm-3.4.src.tar.gz"
+curl "http://llvm.org/releases/3.4/clang-3.4.src.tar.gz"
+tar xfvz llvm-3.4.src.tar.gz
+tar xfvz clang-3.4.src.tar.gz
 
-mv clang llvm/tools/clang
-rm llvm.tar.gz && rm clang.tar.gz
-cd llvm
+mv clang-3.4 llvm-3.4/tools/clang
+# Cleaning up gzip files.
+rm llvm-3.4.src.tar.gz && rm clang-3.4.src.tar.gz
+cd llvm-3.4
 
 # Building and installing - LLVM and clang
 ./configure --enable-shared --enable-optimized --enable-targets=host-only --prefix "$PREFIX"
@@ -18,10 +19,10 @@ make -j$(nproc)
 make install -j$(nproc)
 
 # Installing Pure
-curl "https://github.com/agraef/pure-lang/releases/download/pure-0.68/pure-0.68.tar.gz" -o pure.tar.gz
-tar xzf pure.tar.gz --strip-components=1
-rm pure.tar.gz
-cd pure
+curl -sSL "https://github.com/agraef/pure-lang/releases/download/pure-0.68/pure-0.68.tar.gz"
+tar xfvz pure-0.68.tar.gz
+rm pure-0.68.tar.gz
+cd pure-0.68
 
 # Building and installing pure-lang
 ./configure --enable-release --prefix "$PREFIX"
