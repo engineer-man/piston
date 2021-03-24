eae11d6025bcee046ca7aa2a90931f3890de82c9
Dan Vargas <10914883+dvargas46@users.noreply.github.com>
2021-03-24 15:39:17 -0500

    pkg(cow-1.0.0): Add cow 1.0.0 (#188)
    Co-authored-by: Vargas, Dan <dan.vargas@frostbank.com>

---
+++ packages/cow/1.0.0/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Grab the latest cow source from github
+git clone -q https://github.com/BigZaphod/COW.git cow
+
+# Generate the cow binary into bin
+mkdir -p bin
+sed -i '1i#define NO_GREETINGS' cow/source/cow.cpp
+g++ -o bin/cow cow/source/cow.cpp
+
+# Cleanup
+rm -rf cow
