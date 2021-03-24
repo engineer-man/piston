928c2a6d169b181c98a9b51d009203b32222d83a
Dan Vargas <10914883+dvargas46@users.noreply.github.com>
2021-03-24 15:39:36 -0500

    pkg(pascal-3.2.0): Add pascal (FreePascal) 3.2.0 (#187)

---
+++ packages/pascal/3.2.0/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+PREFIX=$(realpath $(dirname $0))
+
+mkdir -p build
+
+cd build
+curl -L "https://sourceforge.net/projects/freepascal/files/Linux/3.2.0/fpc-3.2.0-x86_64-linux.tar/download"  -o pascal.tar
+tar xf pascal.tar --strip-components=1
+
+# FreePascal uses an interactive installer
+./install.sh << ANSWERS
+$PREFIX
+n
+n
+n
+ANSWERS
+
+cd ..
+rm -rf build
+
+# A sample config (needed for each "project") is written to /etc
+# We'll copy that into the local lib dir (fpc searches there too on compile)
+mkdir lib/fpc/etc
+cp -r /etc/fp* lib/fpc/etc/
