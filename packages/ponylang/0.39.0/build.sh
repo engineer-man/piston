00c6d0e6ae590d32f3c88c56fafe6237f3f4c0cf
Shivansh-007 <shivansh-007@outlook.com>
2021-03-27 16:13:59 +0530

    pkg(pony-0.39.0): Pass `-j` as build flags rather than forcing it to the make command.

--- packages/ponylang/0.39.0/build.sh
+++ packages/ponylang/0.39.0/build.sh
@@ -7,10 +7,10 @@ curl -OL "https://github.com/ponylang/ponyc/archive/refs/tags/0.39.0.tar.gz"
 tar xfz 0.39.0.tar.gz
 
 # Build the vendored LLVM libraries that are included in the `lib/llvm/src`.
-make libs -j$(nproc)
+make libs build_flags="-j$(nproc)"
 # Configure the CMake build directory.
 make configure
 # Will build ponyc and put it in `build/release`.
 make build
 # Install ponyc into `$PREFIX`.
-make prefix="$PREFIX" install -j$(nproc)
+make prefix="$PREFIX" install
