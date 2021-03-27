0cd86741dea1ebd7ede8d372110638f9245b5466
Shivansh-007 <shivansh-007@outlook.com>
2021-03-27 21:27:42 +0530

    pkg(pony-0.39.0): cd into pony dir that was created from tar extraction before building

--- packages/ponylang/0.39.0/build.sh
+++ packages/ponylang/0.39.0/build.sh
@@ -5,12 +5,16 @@ PREFIX=$(realpath $(dirname $0))
 # get sources
 curl -OL "https://github.com/ponylang/ponyc/archive/refs/tags/0.39.0.tar.gz"
 tar xfz 0.39.0.tar.gz
+rm 0.39.0.tar.gz
+
+# cd into pony dir that was created from tar extraction.
+cd 0.39.0
 
 # Build the vendored LLVM libraries that are included in the `lib/llvm/src`.
 make libs build_flags="-j$(nproc)"
 # Configure the CMake build directory.
 make configure
-# Will build ponyc and put it in `build/release`.
+# Will build pony and put it in `build/release`.
 make build
-# Install ponyc into `$PREFIX`.
+# Install pony into `$PREFIX`.
 make prefix="$PREFIX" install
