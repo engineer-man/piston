8d972de264974716804eb26bdfd96845bb80f14c
Shivansh-007 <shivansh-007@outlook.com>
2021-03-30 09:14:15 +0530

    pkg(pony-0.39.0): Reset to 0.39.0 commit and cd into `ponyc`

--- packages/ponylang/0.39.0/build.sh
+++ packages/ponylang/0.39.0/build.sh
@@ -3,13 +3,19 @@
 PREFIX=$(realpath $(dirname $0))
 
 # get sources - only get the latest copy of the relevant files
-git clone --depth 1 https://github.com/ponylang/ponyc.git
+git clone -q https://github.com/ponylang/ponyc.git ponyc
+# release commit for 0.39.0
+git reset --hard 85d897b978c5082a1f3264a3a9ad479446d73984
 
 # updates all submodules recursively along their tracking branches
 # i.e. https://github.com/ponylang/ponyc/blob/main/.gitmodules
 git submodule update --recursive --init
 
+cd ponyc
+
 # build
 make -j$(nproc)
 # Install pony into `$PREFIX`.
 make prefix="$PREFIX" install
+
+rm -rf ponyc
