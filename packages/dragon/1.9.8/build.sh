799b686fdddc9d4a2e26f9981cf3e1daf5b1a531
Dan Vargas <danvargas46@gmail.com>
2021-04-21 20:58:03 -0500

    pkg(dragon-1.9.8): Remove manpages and self alias

--- packages/dragon/1.9.8/build.sh
+++ packages/dragon/1.9.8/build.sh
@@ -8,7 +8,7 @@ cd build
 curl "https://dragon-lang.org/Dragon_1.9.8_linux(x86_x64).tar.bz2" -o dragon.tar.bz2
 tar xjf dragon.tar.bz2 --strip-components=1
 
-### re-purposed the install script for local install
+### re-purposed the install script for local install and without manpages
 
 mkdir -p ../bin
 cp dragon ../bin/
@@ -16,10 +16,6 @@ chmod +x ../bin/dragon
 
 cp -r runtime ../bin/
 chmod +x ../bin/runtime/bin/java
-
-mkdir -p ../usr/share/man/man8
-cp ./man/dragon ../usr/share/man/man8/dragon.8
-gzip ../usr/share/man/man8/dragon.8
 
 sed -i 's|/bin/runtime/bin/java|$JAVA_RUNTIME|g' ../bin/dragon
 
