e287b8de3fe240c8b9f4d19399a9fd60cdaf7081
Dan Vargas <danvargas46@gmail.com>
2021-04-21 21:21:02 -0500

    pkg(cjam-0.6.5): Run jar file normally and remove self alias

--- packages/cjam/0.6.5/build.sh
+++ packages/cjam/0.6.5/build.sh
@@ -4,15 +4,3 @@
 source ../../java/15.0.2/build.sh
 
 curl -L "https://downloads.sourceforge.net/project/cjam/cjam-0.6.5/cjam-0.6.5.jar?ts=gAAAAABgf6xp4bmSJmNMjz8z23zWVbRgW6mhaFrnxdBe_riqC4rZnhUpXJrQzPpSqhniaoGptfkI_r-tjjBc5326oRmloXIJYQ%3D%3D&r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fcjam%2Ffiles%2Fcjam-0.6.5%2Fcjam-0.6.5.jar%2Fdownload" -o bin/cjam.jar
-
-# Really executable jar
-cat <<EOF > bin/cjam
-#!/bin/sh
-exec java -jar \$0 "\$@"
-
-
-
-EOF
-
-cat bin/cjam.jar >> bin/cjam
-chmod +x bin/cjam
