8424307029af51d4b56873d17cbe0f3bf0b84e59
Dan Vargas <10914883+dvargas46@users.noreply.github.com>
2021-04-21 21:30:20 -0500

    pkg(cjam-0.6.5): Add cjam 0.6.5 (#223)
    * pkg(cjam-0.6.5): Add cjam 0.6.5
    
    * pkg(cjam-0.6.5): Run jar file normally and remove self alias

---
+++ packages/cjam/0.6.5/build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# CJam depends on Java
+source ../../java/15.0.2/build.sh
+
+curl -L "https://downloads.sourceforge.net/project/cjam/cjam-0.6.5/cjam-0.6.5.jar?ts=gAAAAABgf6xp4bmSJmNMjz8z23zWVbRgW6mhaFrnxdBe_riqC4rZnhUpXJrQzPpSqhniaoGptfkI_r-tjjBc5326oRmloXIJYQ%3D%3D&r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fcjam%2Ffiles%2Fcjam-0.6.5%2Fcjam-0.6.5.jar%2Fdownload" -o bin/cjam.jar
