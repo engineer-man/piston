0f8f8a4a27ab92ee7bf95e5fe76ed8d3f8522e60
Dan Vargas <danvargas46@gmail.com>
2021-05-08 14:34:12 -0500

    pkg(julia-1.5.4): switch to build from source

--- packages/julia/1.5.4/build.sh
+++ packages/julia/1.5.4/build.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
-curl -OL https://julialang-s3.julialang.org/bin/linux/x64/1.5/julia-1.5.4-linux-x86_64.tar.gz
-tar zxvf julia-1.5.4-linux-x86_64.tar.gz
-rm julia-1.5.4-linux-x86_64.tar.gz
+# Install location
+PREFIX=$(realpath $(dirname $0))
 
+mkdir -p build
+cd build
+
+# Download and extract Julia source
+curl -L "https://github.com/JuliaLang/julia/releases/download/v1.5.4/julia-1.5.4.tar.gz" -o julia.tar.gz
+tar xzf julia.tar.gz --strip-components=1
+
+# Build
+make -j$(nproc)
+echo "prefix=$PREFIX" > Make.user
+make install -j$(nproc)
+
+# Cleanup
+cd ..
+rm -rf build
