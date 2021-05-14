574087940217cb8e651995e0364ab7a0ea704a8e
Dan Vargas <danvargas46@gmail.com>
2021-05-14 18:20:23 -0500

    pkg(scala-3.0.0): use compile

--- packages/scala/3.0.0/build.sh
+++ packages/scala/3.0.0/build.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
 
-# Scala depends on Java
-mkdir -p java
-cd java
-curl "https://download.java.net/java/GA/jdk15.0.2/0d1cfde4252546c6931946de8db48ee2/7/GPL/openjdk-15.0.2_linux-x64_bin.tar.gz" -o java.tar.gz
-tar xzf java.tar.gz --strip-components=1
-rm java.tar.gz
-cd ..
+# Download and extract JDK8
+curl -L "https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u292-b10/OpenJDK8U-jdk_x64_linux_hotspot_8u292b10.tar.gz" -o jdk.tar.gz
+tar xzf jdk.tar.gz --strip-components=1
+rm jdk.tar.gz
 
-mkdir -p scala
-cd scala
-curl -L "https://github.com/lampepfl/dotty/releases/download/3.0.0-RC1/scala3-3.0.0-RC1.tar.gz" -o scala.tar.gz
+# Download and extract Scala 3
+curl -L "https://github.com/lampepfl/dotty/releases/download/3.0.0/scala3-3.0.0.tar.gz" -o scala.tar.gz
 tar -xzf scala.tar.gz --strip-components=1
 rm scala.tar.gz
-cd ..
