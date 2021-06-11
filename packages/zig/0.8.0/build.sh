54ceff9c878d1f0b27afe4995543b23d1dd581c8
Jacob G-W <jacoblevgw@gmail.com>
2021-06-11 16:56:14 -0400

    update zig to 0.8.0
    zig 0.8.0 got released, so this pr updates it

--- packages/zig/0.8.0/build.sh
+++ packages/zig/0.8.0/build.sh
@@ -3,7 +3,7 @@
 mkdir -p bin
 cd bin/
 
-curl -L "https://ziglang.org/download/0.7.1/zig-linux-x86_64-0.7.1.tar.xz" -o zig.tar.xz
+curl -L "https://ziglang.org/download/0.8.0/zig-linux-x86_64-0.8.0.tar.xz" -o zig.tar.xz
 tar xf zig.tar.xz --strip-components=1
 rm zig.tar.xz
 
