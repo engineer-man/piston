adae6fde2f22fd07cb99f142eee277b526ea7969
Brikaa <brikaaomar@gmail.com>
2021-09-14 13:59:19 +0200

    pkg(dotnet-5.0.201): Added F#.net, F# interactive and VB.net

--- packages/dotnet/5.0.201/build.sh
+++ packages/dotnet/5.0.201/build.sh
@@ -7,8 +7,10 @@ rm dotnet.tar.gz
 # Cache nuget packages
 export DOTNET_CLI_HOME=$PWD
 ./dotnet new console -o cache_application
+./dotnet new console -lang F# -o fs_cache_application
+./dotnet new console -lang VB -o vb_cache_application
 # This calls a restore on the global-packages index ($DOTNET_CLI_HOME/.nuget/packages)
 # If we want to allow more packages, we could add them to this cache_application
 
-rm -rf cache_application
+rm -rf cache_application fs_cache_application vb_cache_application
 # Get rid of it, we don't actually need the application - just the restore
