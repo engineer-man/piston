40fec2c82835a6857e999d60a9032fa8fc51c624
Hydrazer <explodingaerosal@gmail.com>
2021-11-24 20:05:37 -0700

    pkg(befunge93-0.2.0)

---
+++ packages/befunge93/0.2.0/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# source python 2.7
+source ../../python/2.7.18/build.sh
+
+# clone befunge repo
+git clone -q 'https://github.com/programble/befungee' befunge93
+
+# checkout the version 0.2.0
+git checkout tag/v0.2.0
