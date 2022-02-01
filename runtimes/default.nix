5ee28d918a8945224563bdfed293a0ff4b6e5757
Dan Vargas <danvargas46@gmail.com>
2022-01-31 19:35:06 -0700

    add ruby 3.1.0

--- runtimes/default.nix
+++ runtimes/default.nix
@@ -14,4 +14,5 @@ args: {
     "erlang" = import ./erlang.nix args;
     "gawk-awk" = import ./gawk-awk.nix args;
     "openjdk11_headless-java" = import ./openjdk11_headless-java.nix args;
+    "ruby" = import ./ruby.nix args;
 }
