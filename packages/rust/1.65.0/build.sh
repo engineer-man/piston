d23e41c3cd1a6872905a56ae7b488ce130eb8834
pan93412 <pan93412@gmail.com>
2022-11-13 05:11:54 +0000

    pkg(rust-1.65.0): Added Rust 1.65.0

---
+++ packages/rust/1.65.0/build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+curl -OL "https://static.rust-lang.org/dist/rust-1.65.0-x86_64-unknown-linux-gnu.tar.gz"
+tar xzvf rust-1.65.0-x86_64-unknown-linux-gnu.tar.gz
+rm rust-1.65.0-x86_64-unknown-linux-gnu.tar.gz
