e9426d6c03cbd8e7d1fb8cc8d1b8ec25e85abd66
Endercheif <45527309+Endercheif@users.noreply.github.com>
2022-12-14 17:42:55 -0800

    fix: pip install from github

--- packages/samarium/0.3.1/build.sh
+++ packages/samarium/0.3.1/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source ../../python/3.10.0/build.sh
-python3 -m pip install samarium==0.3.1
+python3 -m pip install https://github.com/samarium-lang/Samarium/archive/refs/tags/0.3.1.zip
