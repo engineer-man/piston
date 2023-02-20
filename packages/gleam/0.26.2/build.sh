652e85464c377b5a6fac957c5cad23be5458aee8
Lunarmagpie <Bambolambo0@gmail.com>
2023-02-20 00:48:05 -0500

    Update packages/gleam/0.26.2/build.sh
    Co-authored-by: Endercheif <45527309+Endercheif@users.noreply.github.com>

--- packages/gleam/0.26.2/build.sh
+++ packages/gleam/0.26.2/build.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
 # Install Erlang
-apt-get update
-apt-get install wget -y
-wget https://packages.erlang-solutions.com/erlang-solutions_2.0_all.deb
-dpkg -i erlang-solutions_2.0_all.deb
-apt-get install esl-erlang
+source ../../erlang/23.0.0/build.sh
 
 # Install Gleam
 curl -L "https://github.com/gleam-lang/gleam/releases/download/v0.26.2/gleam-v0.26.2-x86_64-unknown-linux-musl.tar.gz" --output gleam.tar.gz
