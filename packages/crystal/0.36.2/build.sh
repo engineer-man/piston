e6294f523c8ac3002e7e053ff343f5aa2490e192
Lunarmagpie <bambolambo0@gmail.com>
2023-03-09 01:04:02 -0500

    remove crystal test

--- packages/crystal/0.36.2/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-PREFIX=$(realpath $(dirname $0))
-
-curl -L "https://github.com/crystal-lang/crystal/releases/download/0.36.1/crystal-0.36.1-1-linux-x86_64.tar.gz" -o crystal.tar.gz
-tar xzf crystal.tar.gz --strip-components=1
-rm crystal.tar.gz
