319d24390357a7feb2aac0f683e942050e731775
Lunarmagpie <bambolambo0@gmail.com>
2023-03-09 03:18:49 -0500

    install stdlib

--- packages/gleam/0.27.0/build.sh
+++ packages/gleam/0.27.0/build.sh
@@ -7,3 +7,11 @@ source ../../erlang/23.0.0/build.sh
 curl -L "https://github.com/gleam-lang/gleam/releases/download/v0.27.0/gleam-v0.27.0-x86_64-unknown-linux-musl.tar.gz" --output gleam.tar.gz
 mkdir gleam
 tar -xf gleam.tar.gz -C gleam
+
+# Update path
+source ./environment
+
+# Install the standard library
+gleam new project --skip-git
+cd project
+gleam build
