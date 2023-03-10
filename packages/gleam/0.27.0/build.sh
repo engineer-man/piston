295d53f565e2c08a5defa07cc9f8971fd8442728
Lunarmagpie <bambolambo0@gmail.com>
2023-03-10 04:24:53 -0500

    install erlang

--- packages/gleam/0.27.0/build.sh
+++ packages/gleam/0.27.0/build.sh
@@ -4,7 +4,7 @@
 source ../../node/16.3.0/build.sh
 
 # Install Erlang
-# source ../../erlang/23.0.0/build.sh
+source ../../erlang/23.0.0/build.sh
 
 # Install Gleam
 curl -L "https://github.com/gleam-lang/gleam/releases/download/v0.27.0/gleam-v0.27.0-x86_64-unknown-linux-musl.tar.gz" --output gleam.tar.gz
