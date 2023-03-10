c26a424bb090f8498a5071bab0a62761f5ebed7a
Lunarmagpie <bambolambo0@gmail.com>
2023-03-10 15:14:51 -0500

    erlang had skill issue

--- packages/erlang/23.0.0/build.sh
+++ packages/erlang/23.0.0/build.sh
@@ -6,7 +6,7 @@ mkdir -p build
 
 cd build
 
-curl "http://erlang.org/download/otp_src_23.0.tar.gz" -o erlang.tar.gz
+curl "http://www.erlang.org/download/otp_src_23.0.tar.gz" -o erlang.tar.gz
 tar xzf erlang.tar.gz --strip-components=1
 rm erlang.tar.gz
 
