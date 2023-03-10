f6570a79b54a1318bf1a312101bb5b92c24be10f
Lunarmagpie <bambolambo0@gmail.com>
2023-03-10 07:13:38 -0500

    add node support

--- packages/gleam/0.27.0/build.sh
+++ packages/gleam/0.27.0/build.sh
@@ -3,6 +3,9 @@
 # Install Erlang
 source ../../erlang/23.0.0/build.sh
 
+# Install Node
+source ../../node/16.3.0/build.sh
+
 # Install Gleam
 curl -L "https://github.com/gleam-lang/gleam/releases/download/v0.27.0/gleam-v0.27.0-x86_64-unknown-linux-musl.tar.gz" --output gleam.tar.gz
 mkdir gleam
