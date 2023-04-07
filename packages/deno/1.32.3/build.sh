624db9549426320f773fd067ae4d3e252245f3eb
RVG|lory <87276663+LoryPelli@users.noreply.github.com>
2023-04-07 10:17:13 +0200

    Add files via upload

---
+++ packages/deno/1.32.3/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -OL https://github.com/denoland/deno/releases/download/v1.32.3/deno-x86_64-unknown-linux-gnu.zip
+unzip -o deno-x86_64-unknown-linux-gnu.zip
+rm deno-x86_64-unknown-linux-gnu.zip
