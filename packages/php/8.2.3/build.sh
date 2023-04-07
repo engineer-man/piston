91d4d402de94b05fc30fe3262a2a19eebe8cda7b
RVG|lory <87276663+LoryPelli@users.noreply.github.com>
2023-04-07 19:40:10 +0200

    Updated to `PHP 8.2.3`

---
+++ packages/php/8.2.3/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+PREFIX=$(realpath $(dirname $0))
+
+mkdir -p build/php
+cd build
+
+curl "https://www.php.net/distributions/php-8.2.3.tar.gz" -o php.tar.gz
+tar xzf php.tar.gz --strip-components=1 -C php
+
+cd php
+
+
+./configure --prefix "$PREFIX"
+
+make -j$(nproc)
+make install -j$(nproc)
+
+cd ../../
+rm -rf build
