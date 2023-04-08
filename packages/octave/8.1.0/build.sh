7ae50eaef79ef9c9c6d8adaee50cc16c018577bb
RVG|lory <87276663+LoryPelli@users.noreply.github.com>
2023-04-08 13:31:16 +0200

    Updated to `Octave 8.1.0`

---
+++ packages/octave/8.1.0/build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Build octave from source
+PREFIX=$(realpath $(dirname $0))
+
+mkdir -p build
+
+cd build
+
+curl -L "https://ftpmirror.gnu.org/octave/octave-8.1.0.tar.gz" -o octave.tar.gz
+
+tar xzf octave.tar.gz --strip-components=1
+
+# === autoconf based ===
+# Disable support for GUI, HDF5 and Java
+./configure --prefix "$PREFIX" --without-opengl --without-qt --without-x --without-hdf5 --disable-java
+
+make -j$(nproc)
+make install -j$(nproc)
+
+cd ../
+rm -rf build
