825cd586e1517e5f6752614cee7d35ebbed57035
RVG|lory <87276663+LoryPelli@users.noreply.github.com>
2023-04-10 17:11:27 +0200

    Updated to `Erlang 25.3.0`

---
+++ packages/erlang/25.3.0/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+PREFIX=$(realpath $(dirname $0))
+
+mkdir -p build
+
+cd build
+
+curl "http://erlang.org/download/otp_src_25.3.tar.gz" -o erlang.tar.gz
+tar xzf erlang.tar.gz --strip-components=1
+rm erlang.tar.gz
+
+export ERL_TOP=$(pwd)
+./configure --prefix "$PREFIX"
+make -j$(nproc)
+make install -j$(nproc)
+
+cd ..
+
+rm -rf build 
