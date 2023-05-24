a2786b17a413e27645d9f307defc8599bbc413c6
RVG|lory <87276663+LoryPelli@users.noreply.github.com>
2023-05-24 20:47:37 +0200

    Updated to `Poweshell 7.3.4`

---
+++ packages/pwsh/7.3.4/build.sh
@@ -0,0 +1,5 @@
+curl -L https://github.com/PowerShell/PowerShell/releases/download/v7.3.4/powershell-7.3.4-linux-x64.tar.gz -o powershell.tar.gz
+tar zxf powershell.tar.gz
+rm powershell.tar.gz
+
+chmod +x pwsh
